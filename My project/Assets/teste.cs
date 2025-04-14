a783290d5de7aec2ade30d1b9a47475a4c7021e2
namjoon-do-bts <flor.l@escolar.ifrn.edu.br>
2025-04-14 14:30:19 -0300

    vo comitar

--- My project/Assets/teste.cs
+++ My project/Assets/teste.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 public class teste : MonoBehaviour
 {
     carro fusca = new carro();
-    
+    carro gol = new carro();
+
+    private caminhao caminhao = new caminhao();
     
     void Start()
     {
         fusca.AtribuirNome("Fusca de Pai");
         Debug.Log("0 "+ fusca.NomeDoCarro());
-        
-        gol.AtribuirNome()
+        fusca.AtribuirNome("Amarelo");
+        fusca.AtribuirNumero_de_rodas(4);
+        fusca.AtribuirPreco(2050.90f);
+        Debug.Log("0 "+ fusca.NomeDoCarro() +" é "+fusca.NomeDoCarro() + " te");
+        gol.AtribuirNome("gol sla");
     }
     
     void Update()
