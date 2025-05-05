cb03706fd0356baffb6879da04991c50f9636309
namjoon-do-bts <flor.l@escolar.ifrn.edu.br>
2025-05-05 16:22:44 -0300

    inimigu

--- My project/Assets/inimigo.cs
+++ My project/Assets/inimigo.cs
@@ -1,16 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class inimigo : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+public class inimigo : Personagem{
+    /*public void AtribuirArmadura(ArmaduraDoInimigo armadura)
     {
-        
-    }
+    
+        void Start()
+        {
+          //  This.armadura = 
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
-}
+    */}
